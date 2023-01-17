87b1d3cf551701d34cfe7659361608474938bb9e
saurabh yadav <saurabh.sy2001>
2023-01-17 09:09:49 +0530

    initial commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,32 +3,64 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
-    private String bill;
+    private final Boolean isVeg;
+    private boolean cheeseAdded;
+    private boolean toppingAdded;
+    private boolean carryBagAdded;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(this.isVeg)
+           this.price = 300;
+        else
+        {
+           this.price = 400;
+        }
+        System.out.println("Base Price Of The Pizza: " + getPrice());
     }
 
     public int getPrice(){
+
         return this.price;
+    }
+    public void setPrice(int price){
+        this.price +=price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!this.cheeseAdded)
+        {
+           this.cheeseAdded = true;
+           this.price += 80;
+            System.out.println("Extra Cheese Added: 80");
+        }
+
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!this.toppingAdded){
+            this.toppingAdded = true;
+            if(this.isVeg){
+               this.price += 70;
+                System.out.println("Extra Toppings Added: 70");
+            }
+            else {
+                this.price +=120;
+                System.out.println("Extra Toppings Added: 120");
+            }
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!this.carryBagAdded){
+            this.carryBagAdded = true;
+            this.price +=20;
+            System.out.println("Paper bag Added: 20");
+        }
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        return ("Total Price: " + this.getPrice());
     }
 }
